844b5981b36e660da9a0fc0c116ebed17473d248
Владислав Пимошенко <softboy@MacBook-Air-Vladislav.local>
2023-01-22 12:47:37 +0300

    first commit

---
+++ src/components/data/goods.js
@@ -0,0 +1,107 @@
+export const goods = [
+    {
+        id: "watches1",
+        category: "watches",
+        name: "Механические наручные часы Stuhrling 3942.2",
+        poster: "https://www.alltime.ru/obj/catalog/watch/stuhrling/img/big/3942-2.jpg",
+        price: "505"
+    },
+    {
+        id: "watches2",
+        category: "watches",
+        name: "Наручные часы Petite Daniel Wellington DW00100474",
+        poster: "https://www.alltime.ru/obj/catalog/watch/daniel-wellington/img/big/DW00100474.jpg",
+        price: "512"
+    },
+    {
+        id: "watches3",
+        category: "watches",
+        name: "Швейцарские наручные часы Calvin Klein K8M2712N с хронографом",
+        poster: "https://www.alltime.ru/obj/catalog/watch/calvin-klein/img/big/K8M2712N.jpg",
+        price: "1887"
+    },
+    {
+        id: "watches4",
+        category: "watches",
+        name: "Российские золотые наручные часы Ника 1246.0.1.52A с хронографом",
+        poster: "https://www.alltime.ru/obj/catalog/watch/nika/img/big/1246-0-1-52A.jpg",
+        price: "7114"
+    },
+    {
+        id: "watches5",
+        category: "watches",
+        name: "Швейцарские наручные часы Swiss Military Hanowa SMWGA2100402",
+        poster: "https://www.alltime.ru/obj/catalog/watch/swiss-military/img/big/SMWGA2100402.jpg",
+        price: "726"
+    },
+    {
+        id: "watches6",
+        category: "watches",
+        name: "Наручные часы D1 Milano UTNJ01",
+        poster: "https://www.alltime.ru/obj/catalog/watch/d1-milano/img/big/UTNJ01.jpg",
+        price: "801"
+    },
+    {
+        id: "watches7",
+        category: "watches",
+        name: "Швейцарские наручные часы Mathey-Tissot D451O",
+        poster: "https://www.alltime.ru/obj/catalog/watch/mathey-tissot/img/big/D451O.jpg",
+        price: "751"
+    },
+    {
+        id: "watches8",
+        category: "watches",
+        name: "Швейцарские наручные часы Cornavin CO.2012-2005R с хронографом",
+        poster: "https://www.alltime.ru/obj/catalog/watch/cornavin/img/big/CO-2012-2005R.jpg",
+        price: "3042"
+    },
+    {
+        id: "watches9",
+        category: "watches",
+        name: "Японские механические наручные часы Seiko SNK375J1",
+        poster: "https://www.alltime.ru/obj/catalog/watch/seiko/img/big/SNK375J1.jpg",
+        price: "863"
+    },
+    {
+        id: "watches10",
+        category: "watches",
+        name: "Швейцарские механические наручные часы Cuervo y Sobrinos 3101.1ASR",
+        poster: "https://www.alltime.ru/obj/catalog/watch/cuervo-y-sobrinos/img/big/3101-1ASR.jpg",
+        price: "7480"
+    },
+    {
+        id: "watches11",
+        category: "watches",
+        name: "Швейцарские механические наручные часы Louis XVI Majeste-1112",
+        poster: "https://www.alltime.ru/obj/catalog/watch/louisxvi/img/big/Majeste-1112.jpg",
+        price: "2062"
+    },
+    {
+        id: "watches12",
+        category: "watches",
+        name: "Наручные часы Diesel DZ1657",
+        poster: "https://www.alltime.ru/obj/catalog/watch/diesel/img/big/DZ1657.jpg",
+        price: "1011"
+    },
+    {
+        id: "watches13",
+        category: "watches",
+        name: "Наручные часы Guess W1117L1",
+        poster: "https://www.alltime.ru/obj/catalog/watch/guess/img/big/W1117L1.jpg",
+        price: "553"
+    },
+    {
+        id: "watches14",
+        category: "watches",
+        name: "Швейцарские наручные часы Continental 20504-GD101950",
+        poster: "https://www.alltime.ru/obj/catalog/watch/continental/img/big/20504-GD101950.jpg",
+        price: "823"
+    },
+    {
+        id: "watches15",
+        category: "watches",
+        name: "Швейцарские наручные часы Calvin Klein K2G21107",
+        poster: "https://www.alltime.ru/obj/catalog/watch/calvin-klein/img/big/K2G21107.jpg",
+        price: "611"
+    }
+]
